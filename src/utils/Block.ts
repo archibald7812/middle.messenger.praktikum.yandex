10cf707c8ebbb8ebb884864496241198aa7244f8
Владислав Розов <rozovvladislav76@yandex.ru>
2023-04-28 02:09:56 +0300

    Add _removeEvents

--- src/utils/Block.ts
+++ src/utils/Block.ts
@@ -67,6 +67,14 @@ export default class Block<P extends Record<string, any> = any> {
 		});
 	}
 
+	_removeEvents() {
+		const { events = {} } = this.props as P & { events: Record<string, () => void> };
+
+		Object.keys(events).forEach((eventName) => {
+			this._element?.removeEventListener(eventName, events[eventName]);
+		});
+	}
+
 	_registerEvents(eventBus: EventBus) {
 		eventBus.on(Block.EVENTS.INIT, this._init.bind(this));
 		eventBus.on(Block.EVENTS.FLOW_CDM, this._componentDidMount.bind(this));
