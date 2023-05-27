d2ac5d0812783001ec50d81e690f9ae2eae64e06
Владислав Розов <rozovvladislav76@yandex.ru>
2023-05-27 19:48:59 +0300

    Stylelint config fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,17 +1,19 @@
 /* eslint-disable no-undef */
 /* eslint-disable global-require */
 const postcssModules = require('postcss-modules');
+const fs = require('fs');
 
 module.exports = {
 	syntax: 'postcss-scss',
 	plugins: [
 		require('postcss-nested'),
+		require('postcss-simple-vars'),
 		postcssModules({
 			getJSON(cssFileName, json) {
 				const cssName = cssFileName.replace('.css', '.scss');
 				const jsonFileName = `${cssName}.json`;
 				fs.writeFileSync(jsonFileName, JSON.stringify(json));
 			},
-		}),
+		})
 	],
 };
